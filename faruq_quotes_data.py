bf661815364cbd83af121406f0c4bcb3cfa4d6e2
Adam Duke <0soy.fish.studios0@gmail.com>
2024-09-01 17:53:33 +1000

    Create faruq_quotes_data.py

---
+++ faruq_quotes_data.py
@@ -0,0 +1,53 @@
+'''
+Name:           faruq_quotes_data.py
+Version:        1
+Used in:        faruq_quotes.py
+'''
+
+faruq_quotes_list = [
+"Are you ready for some robot carnage?",
+"Blue team, ready?",
+"It’s a battle of engineering!",
+"It’s a battle of the best!",
+"It’s a battle of the bots!",
+"It’s a battle of wits and strength!",
+"It’s a clash of the titans!",
+"It’s a fight for dominance!",
+"It’s a fight for survival!",
+"It’s a fight to the death!",
+"It’s a fight to the finish!",
+"It’s time for some robot action!",
+"It’s time for some robot combat!",
+"It’s time for some robot destruction!",
+"It’s time for some robot mayhem!",
+"It’s time for some robot warfare!",
+"It’s time for the main event!",
+"It’s time to settle the score!",
+"It’s time to unleash the bots!",
+"Ladies and gentlemen, are you ready? It’s Robot Fighting Time!",
+"Let the bot battle begin!",
+"Let the destruction begin!",
+"Let’s get ready to rumble!",
+"Let’s see some metal mayhem!",
+"Let’s see some sparks fly!",
+"Let’s see who has the best bot!",
+"Let’s see who has the best design!",
+"Let’s see who has the best skills!",
+"Let’s see who has the best strategy!",
+"Let’s see who has the best tactics!",
+"Let’s see who has the most powerful bot!",
+"Let’s see who has the toughest bot!",
+"Only one bot will emerge victorious!",
+"Prepare for battle!",
+"Red team, ready?",
+"Robots, activate!",
+"The results from the judges are in.",
+"We have a split decision.",
+"Who will be the king of the ring?",
+"Who will be the last bot standing",
+"Who will be the last bot standing?",
+"Who will be the ultimate champion?",
+"Who will be the ultimate victor?",
+"Who will come out on top?",
+"Who will reign supreme?"
+]
